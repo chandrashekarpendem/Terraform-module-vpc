978297a31f59e1819858086b57f651cfbaaab263
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-11 17:25:26 +0530

    subnets_creation_for_natgw_igw

--- vars.tf
+++ vars.tf
@@ -1,7 +1,6 @@
 variable "cidr_block" {}
 variable "default_vpc_id" {}
 variable "env" {}
-variable "subnets" {}
 variable "availability_zone" {}
 variable "private_subnets" {}
 variable "public_subnets" {}
